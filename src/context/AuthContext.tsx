652a03ea22bf573d6452e138e91796df80b92120
abednego emonena <emonenaabednego@gmail.com>
2025-12-06 18:58:14 +0100

    Add console log for successful login in AuthProvider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -102,6 +102,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const login = async (email: string, password: string) => {
     await api.post("/users/login/", { email, password });
+    console.log("Login successful", email, password);
     await fetchUser();
     router.push("/dashboard");
   };
